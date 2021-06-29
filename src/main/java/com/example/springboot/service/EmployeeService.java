c6b7c9dc82d1ccf49af02c7716a99648f6c4696e
CuongNC <you@example.com>
2021-06-29 22:58:21 +0700

    create employee service interface

---
+++ src/main/java/com/example/springboot/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.example.springboot.service;
+
+import java.util.List;
+
+import com.example.springboot.entity.Employee;
+
+public interface EmployeeService {
+    public List<Employee> retrieveEmployees();
+
+    public Employee getEmployee(long employeeId);
+
+    public Employee saveEmployee(Employee employee);
+
+    public void deleteEmployee(Employee employeeId);
+
+    public void updateEmployee(Employee employee);
+}
