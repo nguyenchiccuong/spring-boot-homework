80af5892187b570f24092d02e32503237fd2bcfd
CuongNC <you@example.com>
2021-06-29 22:59:09 +0700

    implement all service from interface employee service but not doing anything

---
+++ src/main/java/com/example/springboot/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.springboot.service.impl;
+
+import java.util.List;
+
+import com.example.springboot.entity.Employee;
+import com.example.springboot.service.EmployeeService;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Override
+    public List<Employee> retrieveEmployees() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Employee getEmployee(long employeeId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Employee employeeId) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void updateEmployee(Employee employee) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
