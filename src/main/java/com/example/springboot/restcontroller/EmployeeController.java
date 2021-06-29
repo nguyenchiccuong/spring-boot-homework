234eac2208ec9431eb20c4475b8f2956d77b79dc
CuongNC <you@example.com>
2021-06-29 23:14:59 +0700

    create EmployeeController.java

--- src/main/java/com/example/springboot/restcontroller/EmployeeController.java
+++ src/main/java/com/example/springboot/restcontroller/EmployeeController.java
@@ -1,5 +1,53 @@
 package com.example.springboot.restcontroller;
 
+import java.util.List;
+
+import com.example.springboot.entity.Employee;
+import com.example.springboot.service.EmployeeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/employee")
 public class EmployeeController {
-    
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> retrieveEmployees() {
+        return employeeService.retrieveEmployees();
+    }
+
+    @GetMapping(path = "{employeeId}")
+    public Employee getEmployee(@PathVariable("employeeId") long employeeId) {
+        return employeeService.getEmployee(employeeId);
+    }
+
+    @PostMapping
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @DeleteMapping(path = "{employeeId}")
+    public void deleteEmployee(@PathVariable("employeeId") long employeeId) {
+        employeeService.deleteEmployee(employeeId);
+    }
+
+    @PutMapping
+    public void updateEmployee(@RequestBody Employee employee) {
+        employeeService.updateEmployee(employee);
+    }
+
 }
