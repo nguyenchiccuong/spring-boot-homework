3988315e655d65311454b34e90adf23c32008532
CuongNC <you@example.com>
2021-06-29 22:11:44 +0700

    add entity employee.java

--- src/main/java/com/example/springboot/entity/Employee.java
+++ src/main/java/com/example/springboot/entity/Employee.java
@@ -1,5 +1,89 @@
 package com.example.springboot.entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+@Entity
+@Table(name = "Employee")
 public class Employee {
-    
+    @Id
+    // @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(name = "sequence-generator", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+            @Parameter(name = "sequence_name", value = "employee_sequence"),
+            // @Parameter(name = "initial_value", value = "4"),
+            @Parameter(name = "increment_size", value = "1") })
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    @Transient // use this annotations for property of object, not a field
+    private Integer age;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Employee(Long id, String name, String email, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
 }
