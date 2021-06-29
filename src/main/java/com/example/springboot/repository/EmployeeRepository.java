c501a87ba78308c7d9bd3c6d71e9c7bd4c3aa980
CuongNC <you@example.com>
2021-06-29 23:20:03 +0700

    add annotation @repository in employeeRepository.java

--- src/main/java/com/example/springboot/repository/EmployeeRepository.java
+++ src/main/java/com/example/springboot/repository/EmployeeRepository.java
@@ -6,7 +6,9 @@ import com.example.springboot.entity.Employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     @Query("FROM employee emp WHERE emp.email = ?1")
